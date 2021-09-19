256b0bd21673490aee2c0fc787a73950f4a6130a
Alberdam Santiago <alberdam.santiago@gmail.com>
2021-09-19 03:16:12 -0400

    feat: Criação de PokemonItem  componentes e paginas pastas

---
+++ src/components/PokemonItem.tsx
@@ -0,0 +1,12 @@
+function PokemonItem(){
+return(
+
+<div>
+    <h2>Nome do Pokemon</h2>
+</div>
+
+)
+
+}
+
+export default PokemonItem
---
+++ src/pages/App.tsx
@@ -0,0 +1,26 @@
+
+import PokemonItem from "../components/PokemonItem"
+
+function App() {
+  
+
+  return (
+  <div>
+  <h1>Meu site com react! </h1>
+  <PokemonItem/>
+  <PokemonItem/>
+  <PokemonItem/>
+  <PokemonItem/>
+  <PokemonItem/>
+  <PokemonItem/>
+  <PokemonItem/>
+  <PokemonItem/>
+  <PokemonItem/>
+  <PokemonItem/>
+  </div>
+  )
+  
+  
+}
+
+export default App;
